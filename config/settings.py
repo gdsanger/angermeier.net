349a144bb19209482362fb9f85a188da87a88563
gdsanger <ca@angermeier.net>
2026-02-23 20:44:11 +0000

    asd

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost', cast=Csv())
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://angermeier.net",
+    "http://localhost:8009",
+    "http://127.0.0.1:8009"
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
